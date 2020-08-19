c9e4be22b18dd726434552bae822b8aedd07c591
xuelei7 <yuxuelei52@hotmail.com>
2020-08-19 13:37:34 +0900

    read csv

---
+++ src/presentdatas.js
@@ -0,0 +1,30 @@
+var dataURL = "../data/clickdata.csv";
+
+//CSVファイルを読み込む関数getCSV()の定義
+function getCSV(){
+    var req = new XMLHttpRequest(); // HTTPでファイルを読み込むためのXMLHttpRrequestオブジェクトを生成
+    req.open("get", dataURL, true); // アクセスするファイルを指定
+    req.send(null); // HTTPリクエストの発行
+	
+    // レスポンスが返ってきたらconvertCSVtoArray()を呼ぶ	
+    req.onload = function(){
+    	convertCSVtoArray(req.responseText); // 渡されるのは読み込んだCSVデータ
+    }
+}
+ 
+// 読み込んだCSVデータを二次元配列に変換する関数convertCSVtoArray()の定義
+function convertCSVtoArray(str){ // 読み込んだCSVデータが文字列として渡される
+    var result = []; // 最終的な二次元配列を入れるための配列
+    //[time(msec), reaction, id]
+    //reaction,,,0;ok,1:NG,2:?,3:smile
+    var tmp = str.split("\n"); // 改行を区切り文字として行を要素とした配列を生成
+ 
+    // 各行ごとにカンマで区切った文字列を要素とした二次元配列を生成
+    for(var i=0;i<tmp.length;++i){
+        result[i] = tmp[i].split(',');
+    }
+    console.log("here");
+    return result;
+}
+ 
+getCSV(); //最初に実行される
